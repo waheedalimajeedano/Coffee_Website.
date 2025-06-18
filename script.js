17b86c3de49a9bf8ae866e20d7441f226fd765ca
Waheed Ali <abdulwaheedmajeedano12690@gmail.com>
2025-06-18 12:35:37 +0500

    Add files via upload
    <!--
      Coffee Shop Website
      Description: A fully responsive, multi-page coffee shop UI design built with HTML, CSS, and JavaScript.
      Pages: Home, About, Menu, Gallery, Testimonials, Contact Us
      Note: Front-end design only; no backend functionality included.
    -->

---
+++ script.js
@@ -0,0 +1,44 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menueCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click",() => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//Close menu when the close button is clicked
+menueCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Initalize swiper
+const swiper = new Swiper('.slider-warpper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+//   Responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+     768: {
+        slidesPerView: 2
+    },
+     1024: {
+        slidesPerView: 3
+    }
+  }
+  
+});
